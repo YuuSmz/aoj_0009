bdb856b97f03b39ecbb4d94dfdc90db7adc4a5c5
YuuSmz <vesagran@gmail.com>
2015-09-24 20:55:29 +0900

    Prime Number
    Count prime number less than n. (n < 1000000)

--- 0009.py
+++ 0009.py
@@ -1,9 +1,10 @@
 import sys
 
-a = [True] * 100000
-for i in range(2, 100000):
-    if (a[i - 1]):
-        for j in range(i ** 2 - 1, 100000, i):
-            a[j] = False
+l = [True] * 1000000
+for i in range(2, 1000000):
+    if (l[i - 1]):
+        for j in range(i ** 2 - 1, 1000000, i):
+            l[j] = False
+
 for s in sys.stdin:
-    print(a[1:int(s)].count(True))
+    print(l[1:int(s)].count(True))
